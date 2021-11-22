a84b97b46c3d0535709c1b1bc1120533beb36fd9
Tori <brunvs21@wfu.edu>
2021-11-22 11:26:20 -0500

    textBasedAdventureGame code

---
+++ src/textBasedAdventureGame.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class textBasedAdventureGame {
+    public class Main{
+        public static void main(String[] args) {
+             // System objects
+            Scanner in = new Scanner(System.in);
+            Random rand = new Random();
+
+            // Game variables
+            String[] enemies = {"Skeleton", "Zombie", "Warrior", "Assassin"};
+            int maxEnemyHealth = 75;
+            int enemyAttachDamage = 25;
+
+
+        }
+    }
+}
